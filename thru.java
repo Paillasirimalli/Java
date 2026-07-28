c7d68eba8ae4a5bca0aafd9b9b4e241e0926c839
Paillasirimalli <paillasiri2007@gmail.com>
2026-07-28 13:53:12 +0530

    Add files via upload

---
+++ thru.java
@@ -0,0 +1,8 @@
+public class thru {
+    public static void main(String args[]) {
+        char gender ='M';
+        String gender2 ="F";
+        System.out.println("Gender "+gender);
+        System.out.println("Gender "+gender2);
+}
+}
